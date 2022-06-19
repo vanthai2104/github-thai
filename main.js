634fc8a1af2a1054e18cbe0b2d6fe694130be4d5
thai <levanthai@gmail.com>
2022-06-19 16:18:08 +0700

    Add csrf protection

---
+++ main.js
@@ -0,0 +1,11 @@
+// khai bao bien 
+var fullName  = 'Le Van Thai';
+ var age = 22; // khai bao tuoi
+
+// goi ham thong bao
+alert(fullName);
+alert(age)
+
+setTimeoutI(function() {
+alert('THONG BAO')
+}, 1000)
